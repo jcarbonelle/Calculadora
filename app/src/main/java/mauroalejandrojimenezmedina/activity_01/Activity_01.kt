32aab7d816863616dff3d18f505307b6f2e356f8
Jorge Herrera <jcarbonelle@uninorte.edu.co>
2018-01-29 17:34:52 -0500

    Initial commit

---
+++ app/src/main/java/mauroalejandrojimenezmedina/activity_01/Activity_01.kt
@@ -0,0 +1,12 @@
+package mauroalejandrojimenezmedina.activity_01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_01 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_01)
+    }
+}
